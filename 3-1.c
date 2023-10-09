2f7df254adb1cbb46a940c616628f9ac525be1d3
RemigayloYegor <remig99@mail.ru>
2023-10-09 23:15:54 +0300

    Задание 3-1

---
+++ 3-1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+// Number 1 
+
+float fun_scan();
+float fun_prim(float x_min);
+
+int main(void) {
+  float prim, d_x, x_min, x_max;
+  x_min = fun_scan();
+  x_max = fun_scan();
+  d_x = fun_scan();
+  while(x_min < x_max){
+    fun_prim(x_min);
+    x_min = x_min + d_x;
+  }
+  return 0;
+}
+
+float fun_prim(float x_min){
+  float prim;
+  prim = 3 * sin(sqrtf(x_min)) + 0.39 * x_min - 3.8;
+  if(prim != prim){
+    printf("%0.0f %s\n",x_min, "Решение отстутствует");
+  } else{
+    printf("%f\n", prim);
+  }
+}
+
+float fun_scan(){
+  int a;
+  float ret;
+  a = scanf("%f", &ret);
+  if (a != 1){
+    printf("%s", "Error");
+  }
+  return ret;
+}
