d5e7a9e428468fcb5faed91fe458c17843ad79f9
RemigayloYegor <remig99@mail.ru>
2023-10-29 23:01:46 +0300

    Update 1-3.c
    Добавил документацию и убрал проверку в отдельную функцию. Переменные не менял, потому что это физические формулы

--- 1-3.c
+++ 1-3.c
@@ -1,30 +1,48 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @brief функция написана, чтобы пользователи вводили значение и проверять его
+ * на то, что он является числом
+ * @param  нет
+ * @return введеное значения пользователя
+ */
 float fun_scan();
 
-int main(){
-    int U = 220, a1, a2;
-    float I1, I2, I, R2;
-    I = fun_scan();
-    I1 = fun_scan();
-    I2 = I - I1;
-    R2 = U / I2;
-    if (I2 >= 0) {
-        printf(" I2 = %0.2f\n A R2 = %0.2f\n", I2, R2);
-    } else {
-        printf("%s", "Error");
-    }
-    return 0;
+/**
+ * @brief Проверяет существования силы тока 2-ой лампы 
+ * @param I - общая сила тока 
+ * @param I1 -  сила тока первой лампы 
+ * @return I2 силу тока второй лампы
+ */
+float fun_check(float I, float I1);
+
+int main() {
+  float I1, I2, I, R2, U = 220;
+  I = fun_scan();
+  I1 = fun_scan();
+  I2 = fun_check(I, I1);
+  R2 = U / I2;
+  printf("%s %f\n %s %f", "Сила тока второй лампы =",I2,"Сопротивление второй лампы =", R2);
 }
 
-float fun_scan(){
-    int a;
-    float b;
-    a = scanf("%f", &b);
-    if (a != 1){
-        printf("%s", "Error");
-    } else {
-        return b;
-    }
-    return 0;
+float fun_scan() {
+  int a;
+  float b;
+  a = scanf("%f", &b);
+  if (a != 1) {
+    abort();
+  } else {
+    return b;
+  }
+  return 0;
+}
+
+float fun_check(float I, float I1) {
+  float I2;
+  I2 = I - I1;
+  if (I2 >= 0) {
+    return I2;
+  } else {
+    abort();
+  }
 }
