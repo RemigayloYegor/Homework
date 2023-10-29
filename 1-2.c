c18166a42b436c8ca1a52b84e3f5251191d029b9
RemigayloYegor <remig99@mail.ru>
2023-10-29 22:00:15 +0300

    Update 1-2.c
    Добавил документацию, изменил переменные, ABORT() и фунции

--- 1-2.c
+++ 1-2.c
@@ -1,32 +1,69 @@
-#include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-//Угол a задан в радианах. Найти его величину в градусах, минутах и секундах.
-
+/**
+ * @brief функция написана, чтобы пользователи вводили значение и проверять его
+ * на то, что он является числом
+ * @param  нет
+ * @return введеное значения пользователя
+ */
 double fun_scan();
+/**
+ * @brief перевод из радианов в градусы
+ * @param  corner - угол
+ * @return угол в градусах
+ */
+double fun_degrees(double corner);
+/**
+ * @brief перевод из радианов в минуты
+ * @param  corner - угол
+ * @return угол в минутах
+ */
+double fun_minutes(double corner);
+/**
+ * @brief перевод из радианов в секунды
+ * @param  corner - угол
+ * @return угол в секундах
+ */
+double fun_seconds(double corner);
 
-int main(){
-    double secu , cor, minu, deg;
-    //cor -  Угол в радианах, deg - Угол в градусах, minu -Угол в минутах, secu - Угол в секундах.
-    cor = fun_scan();
-    // Нахожу угол в градусах по формуле: РАДИАНЫ * (60 * 180) / pi
-    deg = cor * 180 / M_PI;
-    // Нахожу угол в минутах по формуле: РАДИАНЫ * 180 / pi
-    minu = cor * (60 * 180) / M_PI;
-    // Нахожу угол в секундах по формуле: РАДИАНЫ * (3600 * 180) / pi
-    secu = cor * (3600 * 180) / M_PI;
-    printf(" Градусы - %lf\n Минуты - %lf\n Секунды - %lf\"\n", deg, minu, secu);
+int main() {
+  double seconds, corner, minutes, degrees;
+  corner = fun_scan();
+  degrees = fun_degrees(corner);
+  minutes = fun_minutes(corner);
+  seconds = fun_seconds(corner);
+  printf(" Градусы - %lf\n Минуты - %lf\n Секунды - %lf\"\n", degrees, minutes,
+         seconds);
 }
 
-double fun_scan(){
-    int a;
-    double b;
-    a = scanf("%lf", &b);
-    if (a != 1){
-        printf("%s", "Error");
-    } else {
-        return b;
-    }
-    return 0;
+double fun_scan() {
+  int a;
+  double b;
+  a = scanf("%lf", &b);
+  if (a != 1) {
+    abort();
+  } else {
+    return b;
+  }
+  return 0;
+}
+
+double fun_degrees(double corner) {
+  double degrees;
+  degrees = corner * 180 / M_PI;
+  return degrees;
+}
+
+double fun_minutes(double corner) {
+  double minutes;
+  minutes = corner * (60 * 180) / M_PI;
+  return minutes;
+}
+
+double fun_seconds(double corner) {
+  double seconds;
+  seconds = corner * (3600 * 180) / M_PI;
+  return seconds;
 }
