2f9927f6e3f34ebd18042f12cfef1bb619aa49f3
RemigayloYegor <remig99@mail.ru>
2023-11-07 02:27:22 +0300

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -1,39 +1,29 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * @brief Функция расчета формулы
  * @param x,y,z значения параметров
  * @return значение вычеслений
  */
-double fun_calculator1(double x, double y, double z);
+double FunctionA(double x, double y, double z);
 /**
  * @brief Функция расчета формулы
  * @param x,y,z значения параметров
  * @return значение вычеслений
  */
-double fun_calculator2(double x, double y, double z);
+double FunctionB(double x, double y, double z);
 
 int main() {
-  double x, y, z, a, b;
-  x = 0.29;
-  y = 3.7;
-  z = -1;
-  a = fun_calculator1(x, y, z);
-  b = fun_calculator2(x, y, z);
+  double x = 0.29, y = 3.7, z = -1;
   printf("x = %4.2f\n y = %4.2f\n z = %4.2f\n", x, y, z);
-  printf("a = %f\n b = %f\n", a, b);
+  printf("a = %f\n b = %f\n", FunctionA(x, y, z), FunctionB(x, y, z));
 }
 
-double fun_calculator1(double x, double y, double z) {
-  double a;
-  a = 3 * pow(x, y) * log10(y) + exp(z * x);
-  return a;
+double FunctionA(double x, double y, double z) {
+  return 3 * pow(x, y) * log10(y) + exp(z * x);
 }
 
-double fun_calculator2(double x, double y, double z) {
-  double b;
-  b = fabs(2 * y * z) / pow(sin(x), 2) - pow(x, 2) / 3;
-  return b;
+double FunctionB(double x, double y, double z) {
+  return fabs(2 * y * z) / pow(sin(x), 2) - pow(x, 2) / 3;
 }
